070b80c846151349659b02f6bba8e59f0ed39295
Timo Eemeli Koola <timoeemelikoola@Timos-MacBook-Pro.local>
2013-04-07 21:02:32 +0300

    WIP

---
+++ webpage/js/collections/dice.js
@@ -0,0 +1,25 @@
+var app = app || {};
+
+(function () {
+	'use strict';
+
+
+	var DiceList = Backbone.Collection.extend({
+		model: app.Die,
+
+		localStorage: new Backbone.LocalStorage('nodice-backbone'),
+
+		nextOrder: function() {
+			if(!this.length) {
+				return 1;
+			}
+			return this.last().get('order') + 1;
+		},
+
+		comparator: function(die) {
+			return die.get('order');
+		}
+
+	});
+
+})();
---
+++ webpage/js/models/die.js
@@ -0,0 +1,47 @@
+/* global */
+var app = app || {};
+
+(function () {
+	'use strict';
+
+	app.Die = Backbone.Model.extend({
+		defaults: {
+			title: "Die",
+			multiplier: 1,
+			type: 6,
+			modifier: 0
+		},
+
+		modifierPart: function() {
+			var modifier = this.get("modifier");
+			if (modifier === 0) {
+				return "";
+			} else if(modifier >= 0) {
+				return "+" + modifier;
+			} else {
+				return "" + modifier;
+			}
+		},
+		description: function() {
+			var result = "" + this.get("multiplier") + "d" + this.get("type") + this.modifierPart();
+			return result;
+		},
+		minValue: function() {
+			return this.get("multiplier") * 1 + this.get("modifier");
+		},
+		maxValue: function() {
+			return this.get("multiplier")  * this.get("type") + this.get("modifier");
+		},
+		roll: function() {
+			var result = this.get("modifier");
+			for(var i = 0; i < this.get("multiplier"); i++) {
+				result += Math.floor((Math.random() * this.get("type")) + 1);
+			}
+			return result;
+		}
+
+	});
+  
+  
+
+})();
