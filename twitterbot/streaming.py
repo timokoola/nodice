c6af140358d8a27391091078f069066e5ef3cc8c
Timo Koola <timo.kooola@gmail.com>
2013-07-14 00:43:49 +0300

    bot pretty much there

--- twitterbot/streaming.py
+++ twitterbot/streaming.py
@@ -1,25 +1,65 @@
 import tweepy, sys, os
 import argparse # requires 2.7
-import random
+import random, re
 
+
+diere = re.compile('(\d+)[dD](\d+)([+-]?\d*)')
 
 def handle_command_line():
     parser = argparse.ArgumentParser(description="Run streaming twitter bot")
-    parser.add_argument("-t", "--test", help="Get a test response",
-            type=string,default="hello" )
+    parser.add_argument("-t", "--test", help="Get a test response")
     parser.add_argument("-k", "--keyfile", help="Twitter account consumer and accesstokens")
+    parser.add_argument("-u", "--username", help="Username to follow")
     args = parser.parse_args()
     return args
 
 
-class ReplyGenerator:
-	def __init__(self,tweet):
-		self.tweet = tweet
+class Dice:
+    def __init__(self,mth):
+        self.multiplier = int(mth[0])
+        self.type = int(mth[1])
+        if mth[2] == '':
+            self.modifier = 0
+        else:
+            self.modifier = int(mth[2])
 
-	def reply(self):
-		r = random.Random()
-		reply = "@%s %d" % (self.tweet.author.screen_name,r.randint(1,6))
-		return reply
+    def mod_str(self):
+        if self.modifier != 0:
+            return "%+d" % self.modifier
+        else:
+            return ""
+
+    def __repr__(self):
+        return "%dd%d%s" % (self.multiplier,self.type,self.mod_str())
+
+    def roll(self):
+        r = random.Random()
+        val = []
+        for i in xrange(self.multiplier):
+            val.append(r.randint(1,self.type))
+        self.lastRoll = sum(val) + self.modifier
+        self.lastRollStr = "Rolled %d for rolling %s = (%s%s)" % (self.lastRoll, repr(self), "+".join(map(str,val)), self.mod_str())
+
+
+class DiceParser:
+    def __init__(self,description):
+        mth = diere.match(description)
+        if mth:
+            self.die = Dice(mth.groups())
+        else:
+            self.die = Dice([1,6,0])
+
+class ReplyGenerator:
+    def __init__(self,tweet,replyto):
+        self.tweet = tweet
+        self.replyto = replyto
+        dp = DiceParser(tweet)
+        self.dice = dp.die
+        self.dice.roll()
+
+    def reply(self):
+        reply = "@%s %s" % (self.replyto,self.dice.lastRollStr)
+        return reply
 
 
 class CustomStreamListener(tweepy.StreamListener):
@@ -37,9 +77,9 @@ class CustomStreamListener(tweepy.StreamListener):
                                       status.author.screen_name, 
                                       status.created_at, 
                                       status.source,)
-            if status.text.find("@dicenessapp") == 0:
-            	rg = ReplyGenerator(status)
-            	self.tweepyapi.update_status(rg.reply())
+            if status.text.find("@timotestikoola") == 0:
+                rg = ReplyGenerator(status.text,status.author.screen_name)
+                self.tweepyapi.update_status(rg.reply())
 
         except Exception, e:
             print >> sys.stderr, 'Encountered Exception:', e
@@ -73,8 +113,10 @@ class TweepyHelper:
 
 if __name__ == "__main__":
     args = handle_command_line()
-
-    api = (TweepyHelper(args.keyfile)).api
-    api.filter(track=["@dicenessapp"])
-
+    if not args.test:
+        api = (TweepyHelper(args.keyfile)).api
+        api.filter(track=["@timotestikoola"])
+    else:
+        r = ReplyGenerator(args.test, "tester")
+        print r.reply()
 
