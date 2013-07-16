d3053dd0027eec4ad3f34f868db408774e3fd696
Timo Koola <timo.kooola@gmail.com>
2013-07-17 00:39:08 +0300

    release bot 0.8

--- twitterbot/streaming.py
+++ twitterbot/streaming.py
@@ -1,15 +1,18 @@
 import tweepy, sys, os
 import argparse # requires 2.7
 import random, re, daemon, lockfile
+from pymongo import MongoClient
+import json
 
 diere = re.compile('(\d+)[dD](\d+)([+-]?\d*)')
-useraccount = "@timotestikoola"
+useraccount = "@dicenessapp"
+db_collection = None
 
 def handle_command_line():
     parser = argparse.ArgumentParser(description="Run streaming twitter bot")
     parser.add_argument("-t", "--test", help="Get a test response")
     parser.add_argument("-k", "--keyfile", help="Twitter account consumer and accesstokens")
-    parser.add_argument("-u", "--username", help="Username to follow")
+    parser.add_argument("-u", "--username", help="Username to follow", default="@dicenessapp")
     args = parser.parse_args()
     return args
 
@@ -38,23 +41,26 @@ class Dice:
         for i in xrange(self.multiplier):
             val.append(r.randint(1,self.type))
         self.lastRoll = sum(val) + self.modifier
-        self.lastRollStr = "Rolled %d using %s = (%s%s)" % (self.lastRoll, repr(self), "+".join(map(str,val)), self.mod_str())
+        self.lastRollStr = "Rolled %d using %s.\n Individual dice: (%s%s).\nOur roll number %d." % (self.lastRoll, repr(self), "+".join(map(str,val)), self.mod_str(), self.counter)
 
 
 class DiceParser:
-    def __init__(self,description):
+    def __init__(self,description,prev):
         mth = diere.match(description)
+        if not mth:
+             mth = diere.match(prev)
         if mth:
             self.die = Dice(mth.groups())
         else:
             self.die = Dice([1,6,0])
 
 class ReplyGenerator:
-    def __init__(self,tweet,replyto):
+    def __init__(self,tweet,prev,counter):
         self.tweet = tweet
-        self.replyto = replyto
-        dp = DiceParser(tweet.split()[1])
+        self.prev = prev
+        dp = DiceParser(tweet.split()[1],prev)
         self.dice = dp.die
+        self.dice.counter = counter
         self.dice.roll()
 
     def reply(self):
@@ -77,9 +83,26 @@ class CustomStreamListener(tweepy.StreamListener):
                                       status.author.screen_name, 
                                       status.created_at, 
                                       status.source,)
+            #yeah, needs refactoring
+            s = {}
+            s["text"] = status.text
+            s["name"] = status.author.screen_name
+            s["timestamp"] = status.created_at
+            s["source"] = status.source
+            items = db_collection.find({"counter":{"$exists": "true"}})
+            l = [5]
+            try:
+                while True:
+                    item = items.next()
+                    l.append(item["counter"])
+            except StopIteration:
+                counter = max(l)
+            s["counter"] = counter + 1
             if status.text.find(useraccount) == 0:
-                rg = ReplyGenerator(status.text,status.author.screen_name)
+                rg = ReplyGenerator(status.text,item["last_roll"], counter)
+                s["last_roll"] = repr(rg.dice)
                 self.tweepyapi.update_status(rg.reply())
+            db_collection.insert(s)
 
         except Exception, e:
             print >> sys.stderr, 'Encountered Exception:', e
@@ -113,15 +136,23 @@ def run(api):
     api.filter(track=[useraccount])
 
 
+def connect_to_db():
+    global db_collection
+    client = MongoClient()
+    db = client.dicestream
+    db_collection = db.dicecollection
+
 
 if __name__ == "__main__":
     args = handle_command_line()
+    connect_to_db()
+    useraccount = args.username
     api = (TweepyHelper(args.keyfile)).api
-    ferr = open("streamingerrs.txt", "w+")
+    ferr = open("runningstreamingerrs.txt", "w+")
+    fout = open("runningstreamingout.txt", "w+")
     if not args.test:
-        with daemon.DaemonContext(pidfile=lockfile.FileLock("streamingDaemon"), files_preserve=[ferr], stderr=ferr):
+        with daemon.DaemonContext(pidfile=lockfile.FileLock("streamingDaemon"), files_preserve=[ferr, fout], stderr=ferr, stdout=fout):
             run(api)
     else:
-        r = ReplyGenerator(args.test, "tester")
-        print r.reply()
+       run(api)
 
